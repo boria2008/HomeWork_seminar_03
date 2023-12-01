539e12c51e1c7be4630b04bdb9d9259a32884812
Boris_Razor <boria2008@inbox.ru>
2023-12-01 17:27:49 +0300

    Добавил решение третьей задачи.

---
+++ task_03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью.
+//  Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+double[] array = new double[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Math.Round(new Random().NextDouble() * new Random().Next(1, 100), 3);
+}
+Console.WriteLine($"[{string.Join(";\t", array)}]");
+double min = array[0];
+double max = array[1];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+    else if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine($"min={min},\tmax={max}");
+Console.WriteLine($"diff={Math.Round(max - min, 3)}");
