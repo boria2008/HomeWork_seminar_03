bbaf5efa829ba854612cf3b0b3ecfe21e13a339c
Boris_Razor <boria2008@inbox.ru>
2023-12-01 11:12:57 +0300

    Добавил решение второй задачи к третьему семинару

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,8 +1,19 @@
 ﻿// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, 
 // которая определяет количество чётных чисел в массиве.
+
 Console.Clear();
-int []array=new int[10];
+int[] array = new int[10];
 for (int i = 0; i < array.Length; i++)
 {
-    
+    array[i] = new Random().Next(1000);
 }
+Console.WriteLine(string.Join(",", array));
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чётных элементов массива={count}");
